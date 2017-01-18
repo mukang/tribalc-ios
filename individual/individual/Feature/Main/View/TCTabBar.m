7812920f36baa46bf2ffcfb21b92a956461ae3e4
穆康 <mukang@buluo-gs.com>
2017-01-18 11:14:42 +0800

    [更新] 修复“附近”按钮超出TabBar的部分点击时“附近“按钮不会响应问题

--- individual/individual/Feature/Main/View/TCTabBar.m
+++ individual/individual/Feature/Main/View/TCTabBar.m
@@ -27,6 +27,15 @@ NSString *const TCVicinityButtonDidClickNotification = @"TCVicinityButtonDidClic
     return self;
 }
 
+/*
+ {
+ NSColor = "UIExtendedSRGBColorSpace 0.262745 0.262745 0.262745 1";
+ NSFont = "<UICTFont: 0x7fc5c770a610> font-family: \".SFUIText\"; font-weight: normal; font-style: normal; font-size: 10.00pt";
+ NSParagraphStyle = "Alignment 4, LineSpacing 0, ParagraphSpacing 0, ParagraphSpacingBefore 0, HeadIndent 0, TailIndent 0, FirstLineHeadIndent 0, LineHeight 0/0, LineHeightMultiple 0, LineBreakMode 4, Tabs (\n    28L,\n    56L,\n    84L,\n    112L,\n    140L,\n    168L,\n    196L,\n    224L,\n    252L,\n    280L,\n    308L,\n    336L\n), DefaultTabInterval 0, Blocks (\n), Lists (\n), BaseWritingDirection -1, HyphenationFactor 0, TighteningForTruncation NO, HeaderLevel 0";
+ NSShadow = "NSShadow {0, -1} color = {(null)}";
+ }
+ */
+
 - (void)setupSubviews {
     self.translucent = NO;
     
@@ -47,7 +56,7 @@ NSString *const TCVicinityButtonDidClickNotification = @"TCVicinityButtonDidClic
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-//    TCLog(@"%@", [self.selectedItem titleTextAttributesForState:UIControlStateSelected]);
+    
     CGFloat buttonW = self.width / 5.0;
     CGFloat buttonH = self.height;
     int buttonIndex = 0;
@@ -66,6 +75,19 @@ NSString *const TCVicinityButtonDidClickNotification = @"TCVicinityButtonDidClic
     self.vicinityButton.size = self.vicinityButton.currentBackgroundImage.size;
     self.vicinityButton.center = CGPointMake(self.width * 0.5, self.height * 0.5 - 15.6);
     self.titleLabel.center = CGPointMake(self.width * 0.5, CGRectGetMaxY(self.vicinityButton.frame) + margin);
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    if (!self.isHidden) {
+        CGPoint newPoint = [self convertPoint:point toView:self.vicinityButton];
+        if ([self.vicinityButton pointInside:newPoint withEvent:event]) {
+            return self.vicinityButton;
+        } else {
+            return [super hitTest:point withEvent:event];
+        }
+    } else {
+        return [super hitTest:point withEvent:event];
+    }
 }
 
 - (void)handleClickVicinityButton:(UIButton *)sender {
