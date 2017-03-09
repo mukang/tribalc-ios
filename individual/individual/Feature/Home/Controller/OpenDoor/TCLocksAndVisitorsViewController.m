1f40b2b3c35ed3707086e3af004b8a5974c31672
wangshuaifeng <wangshuaifeng@buluo-gs.com>
2017-03-09 19:20:18 +0800

    [更新]LocksAndVisitors页UI创建

--- individual/individual/Feature/Home/Controller/OpenDoor/TCLocksAndVisitorsViewController.m
+++ individual/individual/Feature/Home/Controller/OpenDoor/TCLocksAndVisitorsViewController.m
@@ -7,10 +7,17 @@
 //
 
 #import "TCLocksAndVisitorsViewController.h"
+#import <Masonry.h>
 
-@interface TCLocksAndVisitorsViewController ()
+@interface TCLocksAndVisitorsViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @property (assign, nonatomic) TCLocksOrVisitors locksOrVisitors;
+
+@property (strong, nonatomic) UITableView *tableView;
+
+@property (strong, nonatomic) UIImageView *imageView;
+
+@property (strong, nonatomic) UIButton *addBtn;
 
 @end
 
@@ -29,7 +36,46 @@
 }
 
 - (void)setUpViews {
+    [self.view addSubview:self.imageView];
+    [self.view addSubview:self.tableView];
+    [self.view addSubview:self.addBtn];
     
+    [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    [self.addBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+    }];
+    
+}
+
+- (UIImageView *)imageView {
+    if (_imageView == nil) {
+        _imageView = [[UIImageView alloc] init];
+    }
+    return _imageView;
+}
+
+- (UITableView *)tableView {
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.layer.cornerRadius = 5.0;
+        _tableView.layer.borderColor = TCRGBColor(186, 186, 186).CGColor;
+        CGFloat scale = TCScreenWidth > 375.0 ? 3 : 2;
+        _tableView.layer.borderWidth = 1 / scale;
+        _tableView.clipsToBounds = YES;
+    }
+    return _tableView;
+}
+
+- (UIButton *)addBtn {
+    if (_addBtn == nil) {
+        _addBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    }
+    return _addBtn;
 }
 
 - (void)didReceiveMemoryWarning {
