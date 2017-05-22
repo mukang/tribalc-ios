4b89430c30aa033b08e5a1f796107602af471f01
wangshuaifeng <wangshuaifeng@buluo-gs.com>
2017-05-22 15:10:49 +0800

    [更新] 签到历史记录页面初始化

--- individual/individual/Feature/Profile/Controller/SignIn/TCSignInHistoryViewController.h
+++ individual/individual/Feature/Profile/Controller/SignIn/TCSignInHistoryViewController.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 杭州部落公社科技有限公司. All rights reserved.
 //
 
-#import <TCCommonLibs/TCCommonLibs.h>
+#import <TCCommonLibs/TCBaseViewController.h>
 
 @interface TCSignInHistoryViewController : TCBaseViewController
 
--- individual/individual/Feature/Profile/Controller/SignIn/TCSignInHistoryViewController.m
+++ individual/individual/Feature/Profile/Controller/SignIn/TCSignInHistoryViewController.m
@@ -8,14 +8,23 @@
 
 #import "TCSignInHistoryViewController.h"
 #import <FSCalendar/FSCalendar.h>
+#import "TCBuluoApi.h"
 
-@interface TCSignInHistoryViewController ()
+#define navBarH     64.0
+
+@interface TCSignInHistoryViewController ()<FSCalendarDelegate,FSCalendarDataSource>
+
+@property (weak, nonatomic) UINavigationBar *navBar;
+
+@property (weak, nonatomic) UINavigationItem *navItem;
 
 @property (strong, nonatomic) UIImageView *bgImageView;
 
 @property (strong, nonatomic) UIImageView *topBgImageView;
 
 @property (strong, nonatomic) FSCalendar *calendar;
+
+@property (strong, nonatomic) TCSigninRecordMonth *signinRecordMonth;
 
 @end
 
@@ -25,6 +34,47 @@
     [super viewDidLoad];
     
     [self setUpSubviews];
+    [self setupNavBar];
+    [self loadData];
+}
+
+- (void)loadData {
+    @WeakObj(self)
+    [[TCBuluoApi api] fetchSigninRecordMonth:^(TCSigninRecordMonth *signinRecordMonth, NSError *error) {
+        @StrongObj(self)
+        if (signinRecordMonth) {
+            self.signinRecordMonth = signinRecordMonth;
+            [self.calendar reloadData];
+        }else {
+            NSString *reason = error.localizedDescription ?: @"请稍后再试";
+            [MBProgressHUD showHUDWithMessage:[NSString stringWithFormat:@"加载失败，%@", reason]];
+        }
+    }];
+}
+
+- (void)setupNavBar {
+    self.hideOriginalNavBar = YES;
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    UINavigationBar *navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, self.view.width, navBarH)];
+    [navBar setShadowImage:[UIImage imageNamed:@"TransparentPixel"]];
+    [navBar setBackgroundImage:[UIImage imageNamed:@"TransparentPixel"] forBarMetrics:UIBarMetricsDefault];
+    [self.view addSubview:navBar];
+    
+    UINavigationItem *navItem = [[UINavigationItem alloc] initWithTitle:@"我的签到"];
+    navBar.titleTextAttributes = @{
+                                   NSFontAttributeName : [UIFont systemFontOfSize:16],
+                                   NSForegroundColorAttributeName : [UIColor whiteColor]
+                                   };
+    UIImage *backImage = [[UIImage imageNamed:@"nav_back_item"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    navItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:backImage
+                                                                 style:UIBarButtonItemStylePlain
+                                                                target:self
+                                                                action:@selector(handleClickBackButton:)];
+    [navBar setItems:@[navItem]];
+    
+    self.navBar = navBar;
+    self.navItem = navItem;
 }
 
 - (void)setUpSubviews {
@@ -42,14 +92,43 @@
     [self.calendar mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view);
         make.width.height.equalTo(@300);
-        make.top.equalTo(self.view).offset(200);
+        make.top.equalTo(self.view).offset(250);
     }];
     
+}
+
+#pragma mark FSCalendarDataSource
+- (UIImage *)calendar:(FSCalendar *)calendar imageForDate:(NSDate *)date {
+    if ([[NSCalendar currentCalendar] isDateInToday:date]) {
+        return [UIImage imageNamed:@""];
+    }
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    NSString *str = [formatter stringFromDate:date];
+    
+    return nil;
+}
+
+
+#pragma mark FSCalendarDelegate
+
+- (BOOL)calendar:(FSCalendar *)calendar shouldSelectDate:(NSDate *)date atMonthPosition:(FSCalendarMonthPosition)monthPosition {
+    return NO;
 }
 
 - (FSCalendar *)calendar {
     if (_calendar == nil) {
         _calendar = [[FSCalendar alloc] init];
+        _calendar.delegate = self;
+        _calendar.dataSource = self;
+        _calendar.scrollEnabled = NO;
+        _calendar.appearance.headerDateFormat = @"yyyy年MM月";
+        _calendar.appearance.headerTitleColor = TCBlackColor;
+        _calendar.appearance.caseOptions = FSCalendarCaseOptionsWeekdayUsesSingleUpperCase;
+        _calendar.appearance.weekdayTextColor = TCBlackColor;
+        _calendar.placeholderType = FSCalendarPlaceholderTypeNone;
+        _calendar.scrollDirection = FSCalendarScrollDirectionVertical;
     }
     return _calendar;
 }
