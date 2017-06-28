af5b37fb7412a7e540f5e08a8dd6262ee78f3483
穆康 <mukang@buluo-gs.com>
2017-06-28 16:11:02 +0800

    [新增] 新增TCRentPayViewController

---
+++ individual/individual/Feature/Profile/View/Rent/TCRentPayTabView.h
@@ -0,0 +1,15 @@
+//
+//  TCRentPayTabView.h
+//  individual
+//
+//  Created by 穆康 on 2017/6/28.
+//  Copyright © 2017年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCRentPayTabView : UIView
+
+
+
+@end
