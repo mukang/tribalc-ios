f7d70f772dad1f13e69782cc7867f3017c52c776
穆康 <mukang@buluo-gs.com>
2017-07-21 19:22:26 +0800

    [新增] 新增TCWalletCreditLimitView

---
+++ individual/individual/Feature/Profile/View/Wallet/TCWalletCreditLimitView.h
@@ -0,0 +1,13 @@
+//
+//  TCWalletCreditLimitView.h
+//  individual
+//
+//  Created by 穆康 on 2017/7/21.
+//  Copyright © 2017年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCWalletCreditLimitView : UIView
+
+@end
